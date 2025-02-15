a7cfbcba1e458c8b86d572e37b936b4cce5d90fd
AminRsh <amin.sharifi45@gmail.com>
2025-02-15 13:27:11 -0500

    first_commit

---
+++ src/components/About.jsx
@@ -0,0 +1,62 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+
+import AnimatedTitle from "./AnimatedTitle";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const About = () => {
+  useGSAP(() => {
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#clip",
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
+
+    clipAnimation.to(".mask-clip-path", {
+      width: "100vw",
+      height: "100vh",
+      borderRadius: 0,
+    });
+  });
+
+  return (
+    <div id="about" className="min-h-screen w-screen">
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          Welcome to My Creative World
+        </p>
+
+        <AnimatedTitle
+          title="Expl<b>o</b>re Creativity <br /> Beyond Conventional Bound<b>a</b>ries"
+          containerClass="mt-5 !text-black text-center"
+        />
+
+        <div className="about-subtext">
+          <p>Creating impactful visuals that bring ideas to life with creativity</p>
+          <p className="text-gray-500">
+          Discover a world of creativity through bold designs that inspire, engage, and bring ideas to life. 
+          </p>
+        </div>
+      </div>
+
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="img/about.jpg"
+            alt="Background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
